95b2d53f0c241b538fdf35dd87df1fe22e5396e8
Daniel Richter <danielstefanrichter1@gmail.com>
2024-12-12 16:44:16 +1100

    Add primary colour

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#F8AB53'
+      }
+    },
   },
   plugins: [],
 }
