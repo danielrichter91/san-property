a4e0b7511c65641c2c9d08df20f5cf5ed5b9ac3e
Daniel Richter <danielstefanrichter1@gmail.com>
2024-12-12 14:44:11 +1100

    Add base path

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/san-property/', // Replace 'san-property' with your repository name
+  build: {
+    outDir: 'dist',
+  },
+});
