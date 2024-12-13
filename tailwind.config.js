b7d97e61e4cff09d33cccabbea20ca00e3f90128
Daniel Richter <danielstefanrichter1@gmail.com>
2024-12-13 17:16:23 +1100

    Add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,20 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['InterVariable', ...defaultTheme.fontFamily.sans],
+      },
       colors: {
         primary: '#F8AB53'
-      }
+      },
     },
   },
   plugins: [],
-}
+};
